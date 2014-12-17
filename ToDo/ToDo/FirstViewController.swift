43679918ea86b6da70bc09ec7a5c79cfd41d70b7
Gergely Revay <gergely.revay@gmail.com>
2014-12-17 08:30:23 -0800

    sync Todo app bare minimum

--- ToDo/ToDo/FirstViewController.swift
+++ ToDo/ToDo/FirstViewController.swift
@@ -8,8 +8,22 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate {
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        println("create table length")
+        return (NSUserDefaults.standardUserDefaults().objectForKey("todoId") as String).toInt()!
+        
+    }
+    
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        println("printing cell")
+        var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        cell.textLabel?.text = NSUserDefaults.standardUserDefaults().objectForKey(String(indexPath.row)) as? String
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- ToDo/ToDo/SecondViewController.swift
+++ ToDo/ToDo/SecondViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBAction func editingDone(sender: AnyObject) {
+        if Title.text == "" {
+            ErrorOut.text = "You need to set the title at least!"
+        } else {
+            var todoId:Int = 0
+            if NSUserDefaults.standardUserDefaults().objectForKey("todoId") != nil {
+                var todoIdString = NSUserDefaults.standardUserDefaults().objectForKey("todoId") as String
+                todoId = todoIdString.toInt()! + 1
+            }
+            NSUserDefaults.standardUserDefaults().setObject(Title.text, forKey: String(todoId))
+            NSUserDefaults.standardUserDefaults().setValue(String(todoId), forKey: "todoId")
+            NSUserDefaults.standardUserDefaults().synchronize()
+            println("todo saved")
+        }
+    }
+    
+    @IBOutlet weak var ErrorOut: UILabel!
+    @IBOutlet weak var Description: UITextField!
+    @IBOutlet weak var Title: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
